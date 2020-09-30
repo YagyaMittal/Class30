765243c324723099f384f91c291ef9c13e3485c0
YagyaMittal <yagyamittal02@gmail.com>
2020-09-30 13:54:43 +0530

    we have added vanishing pigs effect and we have added bird reattach to the slingshot

--- Slingshot.js
+++ Slingshot.js
@@ -17,6 +17,9 @@ class SlingShot{
     fly(){
         this.sling.bodyA = null;
     }
+    attach(body){
+        this.sling.bodyA = body;
+    }
 
     display(){
         image(this.sling1,200,20);
